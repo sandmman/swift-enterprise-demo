19bcb25d8730d48a34d7865f73b51b243fded908
Jim Avery <jlavery@us.ibm.com>
2017-01-13 16:43:56 -0600

    Adding ALERT_TOKEN parameter, which we need for Travis

--- Package.swift
+++ Package.swift
@@ -6,5 +6,5 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 8),
-        .Package(url: "https://github.com/IBM-Swift/alert-notification-sdk.git", majorVersion: 0)
+        .Package(url: "https://$ALERT_TOKEN@github.com/IBM-Swift/alert-notification-sdk.git", majorVersion: 0)
 ])
