58f2f873132574d56862f7d5d47601f7623d4e8d
Jim Avery <jlavery@us.ibm.com>
2017-01-16 10:16:00 -0600

    Getting environment variable for the Alert Notification SDK GitHub token

--- Package.swift
+++ Package.swift
@@ -1,4 +1,10 @@
+import Foundation
 import PackageDescription
+
+var tokenString = ""
+if let envToken = ProcessInfo.processInfo.environment["ALERT_TOKEN"] {
+    tokenString = "\(envToken)@"
+}
 
 let package = Package(
     name: "swift-enterprise-demo",
@@ -6,5 +12,5 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 8),
-        .Package(url: "https://$ALERT_TOKEN@github.com/IBM-Swift/alert-notification-sdk.git", majorVersion: 0)
+        .Package(url: "https://\(tokenString)github.com/IBM-Swift/alert-notification-sdk.git", majorVersion: 0)
 ])
