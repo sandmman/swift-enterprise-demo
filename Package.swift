1c375c7c7e2c72ff424d8dce0e9208606643cb3e
Jim Avery <jlavery@us.ibm.com>
2017-01-19 10:19:10 -0600

    Pushing the manual include of SwiftMetrics

--- Package.swift
+++ Package.swift
@@ -4,12 +4,15 @@ let package = Package(
     name: "swift-enterprise-demo",
     targets: [
         Target(name: "CloudFoundryConfiguration", dependencies: []),
-        Target(name: "swift-enterprise-demo", dependencies: ["CloudFoundryConfiguration"])
+        Target(name: "SwiftMetrics", dependencies: []),
+        Target(name: "SwiftMetricsKitura", dependencies: ["SwiftMetrics"]),
+        Target(name: "swift-enterprise-demo", dependencies: ["CloudFoundryConfiguration", "SwiftMetrics", "SwiftMetricsKitura"])
     ],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/RuntimeTools/omr-agentcore.git", majorVersion: 3),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://d7d10e9fbcfb7eb9c9085927777fcdca9a323586@github.com/IBM-Swift/SwiftMetrics.git", majorVersion: 0),
         .Package(url: "https://d7d10e9fbcfb7eb9c9085927777fcdca9a323586@github.com/IBM-Swift/alert-notification-sdk.git", majorVersion: 0)
 ])
