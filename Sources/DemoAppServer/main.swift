574bb0229c62e7f556a97bd0c5a8626af0cf7aeb
rolivieri <roliv@us.ibm.com>
2017-01-19 13:50:54 -0600

    Minor refactoring and renaming

--- Sources/DemoAppServer/main.swift
+++ Sources/DemoAppServer/main.swift
@@ -13,7 +13,7 @@ Log.logger = HeliumLogger()
 
 // Acquire service credentials from the config file.
 let config = try Configuration(withFile: "cloud_config.json")
-guard let alertCredentials = config.getCredentials(forService: "swift-enterprise-demo-alert"),
+guard let alertCredentials = config.getCredentials(forService: "SwiftEnterpriseDemo-Alert"),
     let url = alertCredentials["url"] as? String,
     let name = alertCredentials["name"] as? String,
     let password = alertCredentials["password"] as? String else {
