fc693ef4832454d2d6e0660701df04fb3c9425db
rolivieri <roliv@us.ibm.com>
2017-01-20 13:58:43 -0600

    Added reference to CircuitBreaker library.

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/CircuitBreaker.git", majorVersion: 0, minor: 0),
         .Package(url: "https://d7d10e9fbcfb7eb9c9085927777fcdca9a323586@github.com/IBM-Swift/SwiftMetrics.git", majorVersion: 0),
         .Package(url: "https://d7d10e9fbcfb7eb9c9085927777fcdca9a323586@github.com/IBM-Swift/alert-notification-sdk.git", majorVersion: 0)
 ])
