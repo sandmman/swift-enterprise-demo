94209487a0b828701f63df4438e5b29493827ddb
Jim Avery <jlavery@us.ibm.com>
2017-01-31 17:00:46 -0600

    Added functional (and visual) tabs

--- public/index.js
+++ public/index.js
@@ -1,6 +1,11 @@
 var angular;
 
 angular.module("demoApp", [])
+.config(config)
 .controller('homeController', homeController)
 .controller('autoScalingController', autoScalingController)
 .controller('circuitBreakerController', circuitBreakerController);
+
+function config($compileProvider) {
+    $compileProvider.debugInfoEnabled(false);
+}
