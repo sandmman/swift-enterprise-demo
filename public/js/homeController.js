3b81c1f205b468cd8767423bf7c84fa2bbd2d16a
Jim Avery <jlavery@us.ibm.com>
2017-02-07 09:35:05 -0600

    Updated with ranges and AppEnv changes

--- public/js/homeController.js
+++ public/js/homeController.js
@@ -1,13 +1,13 @@
 var homeController = function homeController($scope, $http) {
     $scope.displayedTab = "autoScaling";
-    $scope.memoryMax = 256;
+    $scope.memoryMax = 256 * 0.875;
     $scope.memoryStep = 32;
     
-    var getInitData = function getInitData() {
+    $scope.getInitData = function getInitData() {
         $http.get('/initData')
         .then(function onSuccess(response) {
-              {
-                $scope.memoryMax = response.data.memoryMax;
+                //$scope.memoryMax = response.data.memoryMax;
+                console.log(response);
               },
               function onFailure(response) {
                  console.log("Failed to get initial data from server.");
