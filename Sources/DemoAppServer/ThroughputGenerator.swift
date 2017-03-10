386423b72d17e091bae0b6077cd25f3688d99b25
Jim Avery <jlavery@us.ibm.com>
2017-03-10 10:07:01 -0600

    Removing the lock state and using DispatchWorkItem.cancel()

--- Sources/DemoAppServer/ThroughputGenerator.swift
+++ Sources/DemoAppServer/ThroughputGenerator.swift
@@ -15,6 +15,7 @@
  **/
 
 import Foundation
+import LoggerAPI
 import Configuration
 import CloudFoundryEnv
 #if os(Linux)
@@ -22,30 +23,14 @@ import CloudFoundryEnv
 #endif
 
 class ThroughputGenerator {
-    class ThroughputLock {
-        var state: Int8
-
-        init(_ state: Int8) {
-            self.state = state
-        }
-
-        func incrementState() {
-            if self.state >= 100 {
-                self.state = 0
-            } else {
-                self.state += 1
-            }
-        }
-    }
-
-    var lock: ThroughputLock
     var queue: DispatchQueue
     var requestsPerSecond: Int
+    var workItem: DispatchWorkItem?
 
     init() {
-        self.lock = ThroughputLock(0)
         self.queue = DispatchQueue(label: "throughputQueue", attributes: .concurrent)
         self.requestsPerSecond = 0
+        self.workItem = nil
     }
 
     /*func generateBlock(lock: ThroughputLock, lockValue: Int) -> (Timer) -> Void {
@@ -95,8 +80,8 @@ class ThroughputGenerator {
         // Set the field.
         self.requestsPerSecond = requestsPerSecond
         
-        // Increment the lock.
-        self.lock.incrementState()
+        // Cancel previous work items.
+        self.workItem?.cancel()
 
         // If requestsPerSecond is 0 or less, don't bother creating new threads.
         guard requestsPerSecond > 0 else {
@@ -107,34 +92,40 @@ class ThroughputGenerator {
         if configMgr.isLocal == false {
             requestURL = "\(configMgr.url)/requestJSON"
         }
-
-        let currentState = self.lock.state
         
-        for _ in 0..<requestsPerSecond {
-            self.queue.async(execute: {
-                let startDate = Date()
-                var waitDate = Date()
-                let continueState = currentState
-                while startDate.timeIntervalSinceNow > -600 && continueState == self.lock.state {
-                    if waitDate.timeIntervalSinceNow < -1 {
-                        waitDate = Date()
-                        // Make a request, don't worry about the result.
-                        if let requestURL = URL(string: requestURL) {
-                            var cookies: [String: Any] = [:]
-                            if configMgr.isLocal == false, let appData = configMgr.getApp(), let vcap = vcapCookie {
-                                cookies["JSESSIONID"] = "\(appData.instanceIndex)"
-                                cookies["__VCAP_ID__"] = vcap
-                            }
-                            networkRequest(url: requestURL, method: "GET", cookies: cookies) {
-                                data, response, error in
-                                return
-                            }
+        self.workItem = DispatchWorkItem() {
+            let startDate = Date()
+            var waitDate = Date()
+            guard let selfReference = self.workItem else {
+                Log.warning("Worker thread lost reference to work item and will self-destruct.")
+                return
+            }
+            while !selfReference.isCancelled && startDate.timeIntervalSinceNow > -600 {
+                if waitDate.timeIntervalSinceNow < -1 {
+                    waitDate = Date()
+                    // Make a request, don't worry about the result.
+                    if let requestURL = URL(string: requestURL) {
+                        // Set cookies in order to ensure session affinity.
+                        var cookies: [String: Any] = [:]
+                        if configMgr.isLocal == false, let appData = configMgr.getApp(), let vcap = vcapCookie {
+                            cookies["JSESSIONID"] = "\(appData.instanceIndex)"
+                            cookies["__VCAP_ID__"] = vcap
+                        }
+                        networkRequest(url: requestURL, method: "GET", cookies: cookies) {
+                            data, response, error in
+                            return
                         }
                     }
-                    // Sleep for 0.1 seconds.
-                    usleep(100_000)
                 }
-            })
+                // Sleep for 0.1 seconds.
+                usleep(100_000)
+            }
+        }
+        
+        if let workItem = self.workItem {
+            for _ in 0..<requestsPerSecond {
+                self.queue.async(execute: workItem)
+            }
         }
     }
 }
