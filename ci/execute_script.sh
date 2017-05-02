0806ecf5fa5bea1ca8cb1fef8ff22148eafff4dd
rolivieri <roliv@us.ibm.com>
2017-05-02 11:45:18 -0500

    Fixed defect in ci script

--- ci/execute_script.sh
+++ ci/execute_script.sh
@@ -16,14 +16,6 @@
 
 set -e
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  wget http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/Bluemix_CLI_0.5.2_amd64.tar.gz
-  tar -xvf Bluemix_CLI_0.5.2_amd64.tar.gz
-  cd Bluemix_CLI && sudo ./install_bluemix_cli && cd ..
-  bx login -a https://$BLUEMIX_REGION -u $BLUEMIX_USER -p $BLUEMIX_PWD -s applications-dev -o $BLUEMIX_ORGANIZATION
-  TOKEN=$(bx cf oauth-token)
-fi
-
 if [[ $TRAVIS_BRANCH = "master" ]]; then
     echo "Building project with 'production' credentials..."
     ./Package-Builder/build-package.sh -projectDir $TRAVIS_BUILD_DIR -credentialsDir $TRAVIS_BUILD_DIR/Testing-Credentials/SwiftEnterpriseDemo/production
@@ -33,5 +25,10 @@ else
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  wget http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/Bluemix_CLI_0.5.2_amd64.tar.gz
+  tar -xvf Bluemix_CLI_0.5.2_amd64.tar.gz
+  cd Bluemix_CLI && sudo ./install_bluemix_cli && cd ..
+  bx login -a https://$BLUEMIX_REGION -u $BLUEMIX_USER -p $BLUEMIX_PWD -s applications-dev -o $BLUEMIX_ORGANIZATION
+  TOKEN=$(bx cf oauth-token)
   sed -i -e 's/<token>/$TOKEN/' $TRAVIS_BUILD_DIR/cloud_config.json
 fi
